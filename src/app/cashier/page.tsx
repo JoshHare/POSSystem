0d2ba84fe781018f5fb23f97200b706243ee0fab
Christopher Vu <65040991+S1nthesis@users.noreply.github.com>
2023-10-31 15:18:16 -0500

    Revert "navbar cashier frontend"

--- src/app/cashier/page.tsx
+++ src/app/cashier/page.tsx
@@ -1,17 +1,11 @@
-import Link from 'next/link';
 import styles from './page.module.css'
 import Navbar from '../../components/Navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
         <Navbar/>
-        <div className={styles.pContainer}>
-          <p className={styles.pItem}><Link href="/cashier/fruityAndRefreshing">Fruity and Refreshing</Link></p>
-          <p className={styles.pItem}><Link href="/cashier/sweetAndCreamy">Sweet and Creamy</Link></p>
-          <p className={styles.pItem}><Link href="/cashier/coffeeFlavored">Coffee Flavored</Link></p>
-          <p className={styles.pItem}><Link href="/cashier/seasonalDrinks">Seasonal Drinks</Link></p>
-        </div>
-    </div>
+        <p>Customer page (replace later)</p>
+    </>
   )
 }
